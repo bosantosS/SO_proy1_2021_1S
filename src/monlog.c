12b0039868d2ff2409fe534fc0601a0bc1a8750c
bosantosS <bosantossantistevan@gmail.com>
2021-07-17 14:02:47 -0400

    Separates service by ','

--- src/monlog.c
+++ src/monlog.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <getopt.h>
 
+#include <pthread.h>
 
 /* monlog -s servicio1,servicio2 -t 1 */
 void print_help(char *command)
@@ -18,10 +19,43 @@ void print_help(char *command)
 	
 }
 
+/* Parsear los servicios */
+char **parse_services(char *line, char *delimiter){
+	char *token, *saveptr, *saveptr1;
+	char *line_copy;
+	int i, num_tokens = 0;
+	char **services = NULL;
+	
+	line_copy = (char *) malloc(strlen(line) + 1);
+	strcpy(line_copy, line);
+	
+	token = strtok_r(line_copy, delimiter, &saveptr);
+	while(token != NULL){
+		token = strtok_r(NULL, delimiter, &saveptr);
+		num_tokens++;
+	}
+	free(line_copy);
+	
+	if(num_tokens > 0){
+		services = (char **) malloc((num_tokens + 1) * sizeof(char **));
+		token = strtok_r(line, delimiter, &saveptr1);
+		for(i = 0; i < num_tokens; i++){
+			services[i] = (char *) malloc(strlen(token)+1);
+			strcpy(services[i], token);
+			token = strtok_r(NULL, delimiter, &saveptr1);
+		}
+		services[i] = NULL;
+	}
+	
+	return services;
+}
+
+
 int main(int argc, char **argv){
 	int opt,c,i;
 	int seconds=1;
 	char *services_par=NULL;
+	char **services;
 	
 	/* Variables globales de getopt */
   	extern char* optarg;
@@ -29,7 +63,8 @@ int main(int argc, char **argv){
   	extern int optopt;
   	extern int opterr;
   	opterr = 0;
-	
+  	
+  	/* Determinar los argumentos */
 	while((c = getopt(argc, argv, "s:ht:"))!=-1){
 		switch(c){
 			case 's':
@@ -57,12 +92,18 @@ int main(int argc, char **argv){
 			
 		}		
 	}
+	
 	/* Comprobar los services */
 	if(!services_par){
 		fprintf(stderr, "Debe especificar al menos 2 services de forma obligatoria!\n");
 		print_help(argv[0]);	
 	}else{
 		printf("services_par: %s\n", services_par);
+		services = parse_services(services_par, ",");
+		if(services){
+			printf("service_1: %s\n", services[0]);
+			printf("service_2: %s\n", services[1]);
+		}
 	}
 	
 	/* Comprobar el tiempo de actualizacion*/
@@ -74,7 +115,8 @@ int main(int argc, char **argv){
 		printf("Ficheros que se enviaran %s\n", argv[i]);
 	}
 	
-	printf("\n\n");
+	printf(">> START");
+	
 	return 0;
 	
 }
